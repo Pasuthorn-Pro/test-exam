bc6c2b595763c9488a8d368c8a3c302c4227f84c
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-08-22 08:50:38 +0700

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,9 @@
+def find_single_occurence_numbers(numbers: list) -> list:
+    if numbers 
+        return (set(find_single_occurence_numbers))
+    
+
+print(find_single_occurence_numbers([4, 5, 6, 4, 7, 5, 6])) #Output [6, 7, 8]
+print(find_single_occurence_numbers([1, 2, 2, 3 , 3, 4, 4])) #Output [1]
+print(find_single_occurence_numbers([1, 2, 3, 4, 5, 6])) #Output [1, 2, 3, 4, 5, 6]
+print(find_single_occurence_numbers([1, 1, 1, 1])) #Output []
