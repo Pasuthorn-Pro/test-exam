0f44a2782c04286a124ede1f93b06e88aefaf30d
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-08-22 08:59:21 +0700

    test exam

--- 001.py
+++ 001.py
@@ -1,9 +1,7 @@
 def find_single_occurence_numbers(numbers: list) -> list:
-    if numbers 
-        return (set(find_single_occurence_numbers))
+    return [num for num in numbers if numbers.count(num) == 1]
     
-
-print(find_single_occurence_numbers([4, 5, 6, 4, 7, 5, 6])) #Output [6, 7, 8]
+print(find_single_occurence_numbers([4, 5, 6, 4, 7, 5, 8])) #Output [6, 7, 8]
 print(find_single_occurence_numbers([1, 2, 2, 3 , 3, 4, 4])) #Output [1]
 print(find_single_occurence_numbers([1, 2, 3, 4, 5, 6])) #Output [1, 2, 3, 4, 5, 6]
 print(find_single_occurence_numbers([1, 1, 1, 1])) #Output []
